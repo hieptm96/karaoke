644509364ea0e4759428f96e2176128d578910cf
hieptm96 <hiep1996tb@gmail.com>
2017-07-19 18:05:18 +0700

    Ktv management

---
+++ resources/views/ktvs/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Page-Title -->
+    <div class="row">
+        <div class="col-sm-12">
+            <h4 class="page-title">Bài hát</h4>
+            <ol class="breadcrumb">
+                <li>
+                    <a href="#">KTV</a>
+                </li>
+                <li class="active">
+                    Thêm mới
+                </li>
+            </ol>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card-box">
+                <h4 class="m-t-0 header-title"><b>Thêm mới đơn vị kinh doanh</b></h4>
+                <p class="text-muted font-13 m-b-30">
+                </p>
+
+                <form action="{{ route('ktvs.store')  }}" method="POST" class="form-horizontal" role="form" data-parsley-validate="" novalidate="">
+                    @include('ktvs._form')
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
